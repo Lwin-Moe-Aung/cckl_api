32d7fd4ad8a8391dd794b3d9480d9184cf580186
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2022-12-28 10:13:43 +0630

    configuration for window

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,7 @@
 const dbConfig = {
-    HOST: 'cckl_mysql',
-    USER: 'cckl_admin',
-    PASSWORD: 'cckl_root',
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: 'password',
     DB: 'cckl_database',
     dialect: 'mysql',
     pool: {
@@ -12,3 +12,21 @@ const dbConfig = {
     }
 }
 module.exports = dbConfig;
+
+// ----------------------------------------------------------
+
+//* db config for docker compose 
+// const dbConfig = {
+//     HOST: 'cckl_mysql',
+//     USER: 'cckl_admin',
+//     PASSWORD: 'cckl_root',
+//     DB: 'cckl_database',
+//     dialect: 'mysql',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// }
+// module.exports = dbConfig;
--- config/dbConn.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql2"); 
-
-const db = mysql.createConnection({
-    host: "cckl_mysql_server",
-    user: "cckl_admin",
-    password: "cckl_root",
-    database: "cckl_db",
-})
-module.exports = db;
