e0540b963a8915e7f6cbdcc8f2096e66b0f68113
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2023-02-10 22:16:29 +0630

    cors option

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:5173',
-    'http://54.169.192.233'
+    'http://54.169.192.233',
+    'http://ccklos.org',
 ];
 
 module.exports = allowedOrigins;
